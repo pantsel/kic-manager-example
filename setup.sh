8968cc4f655f7327ae85077a38439e393d4d3679
pantsel <panagis.tselentis@konghq.com>
2025-03-05 17:00:29 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,20 @@
+helm repo add kong https://charts.konghq.com
+helm repo update
+
+kubectl create namespace kong --dry-run=client -o yaml | kubectl apply -f -
+helm upgrade --install kong kong/ingress -n kong --values kic.values.yaml
+
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+kubectl apply -f ./manifests/license.yaml
+kybectl apply -f ./manifests/konnect-client-secret.yaml
+kubectl apply -f ./manifests/gateway.yaml
+
+# Deploy the echo service
+kubectl apply -f https://docs.konghq.com/assets/kubernetes-ingress-controller/examples/echo-service.yaml
+
+# Configure Ingresses
+kubectl apply -f ./manifests/routes-echo.yaml
+kubectl apply -f ./manifests/routes-manager.yaml
+
+# Deploy OIDC plugin
+kubectl apply -f ./manifests/plugin-oidc-auth.yaml
